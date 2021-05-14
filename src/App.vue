ad7ccae4a5a9f22740195fc8ebf018c8d0a16381
dkodrin <75334503+dkodrin@users.noreply.github.com>
2021-05-14 13:52:43 +0200

    inicijalni setup

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Homea</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+    <Navbar></Navbar>
   </div>
 </template>
 
-<style lang="scss">
+<script>
+import Navbar from '@/components/Navbar.vue';
+export default{
+  name: "App.vue",
+    components: {
+        Navbar
+    },
+};
+</script>
+
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -19,14 +25,14 @@
 
 #nav {
   padding: 30px;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
